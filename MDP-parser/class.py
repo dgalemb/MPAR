32236fd6e5280c29f50c1e852869ff483f45498a
dgalemb <daniel.galembeck@gmail.com>
2023-02-09 10:32:16 +0100

    Beginning TP

---
+++ MDP-parser/class.py
@@ -0,0 +1,5 @@
+class Etat():
+
+
+    def __init__(self, nom) -> None:
+        self.nom = nom
--- MDP-parser/mdp.py
+++ MDP-parser/mdp.py
@@ -3,18 +3,20 @@ from gramLexer import gramLexer
 from gramListener import gramListener
 from gramParser import gramParser
 import sys
-
         
 class gramPrintListener(gramListener):
 
-    def __init__(self):
+    def __init__(self, dictt = {'transact_type1': [], 'transact_type2': [], 'actions' : [], 'etats': []}):
+        self.props = dictt
         pass
         
     def enterDefstates(self, ctx):
         print("States: %s" % str([str(x) for x in ctx.ID()]))
+        self.props['etats'] = ([str(x) for x in ctx.ID()])
 
     def enterDefactions(self, ctx):
         print("Actions: %s" % str([str(x) for x in ctx.ID()]))
+        self.props['actions'] = [str(x) for x in ctx.ID()]
 
     def enterTransact(self, ctx):
         ids = [str(x) for x in ctx.ID()]
@@ -22,12 +24,16 @@ class gramPrintListener(gramListener):
         act = ids.pop(0)
         weights = [int(str(x)) for x in ctx.INT()]
         print("Transition from " + dep + " with action "+ act + " and targets " + str(ids) + " with weights " + str(weights))
+        self.props['transact_type1'].append([dep, act, ids, weights])
+
         
     def enterTransnoact(self, ctx):
         ids = [str(x) for x in ctx.ID()]
         dep = ids.pop(0)
         weights = [int(str(x)) for x in ctx.INT()]
         print("Transition from " + dep + " with no action and targets " + str(ids) + " with weights " + str(weights))
+        self.props['transact_type2'].append([dep, ids, weights])
+
 
             
 def main():
@@ -37,7 +43,9 @@ def main():
     tree = parser.program()
     printer = gramPrintListener()
     walker = ParseTreeWalker()
-    walker.walk(printer, tree)
+    p = walker.walk(printer, tree)
+
+    print(printer.props)
 
 if __name__ == '__main__':
     main()
