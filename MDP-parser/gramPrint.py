7dd3c5f43f3367c305f1d82cdcc78b3bdf221cc8
nucontreras <48302692+nucontreras@users.noreply.github.com>
2023-02-26 19:55:19 +0100

    colors and actions added

--- MDP-parser/gramPrint.py
+++ MDP-parser/gramPrint.py
@@ -4,81 +4,12 @@ import matplotlib.pyplot as plt  # For plotting
 
 import numpy as np
 import pandas as pd
-import networkx as nx
-from networkx.drawing.nx_agraph import write_dot
+# import networkx as nx
+# from networkx.drawing.nx_agraph import write_dot
 import matplotlib.pyplot as pl
 
 def print_graph():
-    # create state space and initial state probabilities
-    states = [(0, 0),
-          (1, 0),
-          (2, 0),
-          (3, 0),
-          (4, 0),
-          (0, 1),
-          (1, 1),
-          (2, 1),
-          (3, 1),
-          (4, 1),
-          (0, 2),
-          (1, 2),
-          (2, 2),
-          (3, 2),
-          (4, 2),
-          (0, 3),
-          (1, 3),
-          (2, 3),
-          (3, 3),
-          (0, 4),
-          (1, 4),
-          (2, 4)]
-    Q = [[-5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [1/2, -6, 5, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 1, -7, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 1, -7, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 1, -2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [1/5, 0, 0, 0, 0, -26/5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 1/5, 0, 0, 0, 1/2, -31/5, 5, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 1/5, 0, 0, 0, 1, -36/5, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 1/5, 0, 0, 0, 1, -36/5, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 1/5, 0, 0, 0, 1, -11/5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 2/5, 0, 0, 0, 0, -27/5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 1/2, -32/5, 5, 0, 0, 1/2, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 1, -37/5, 5, 0, 0, 1, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 1, -37/5, 5, 0, 0, 1, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 1, -12/5, 0, 0, 0, 1, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 0, -27/5, 5, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 1/2, -32/5, 5, 0, 1/2, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 1/2, -32/5, 5, 0, 1/2, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, 1/2, -7/5, 0, 0, 1/2],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, -27/5, 5, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, -27/5, 5],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2/5, 0, 0, 0, -2/5]]
-
-    G = nx.MultiDiGraph()
-    labels={}
-    edge_labels={}
-
-    for i, origin_state in enumerate(states):
-        for j, destination_state in enumerate(states):
-            rate = Q[i][j]
-            if rate > 0:
-                G.add_edge(origin_state,
-                           destination_state,
-                           weight=rate,
-                           label="{:.02f}".format(rate))
-                edge_labels[(origin_state, destination_state)] = label="{:.02f}".format(rate)
-
-    plt.figure(figsize=(14,7))
-    node_size = 200
-    pos = {state:list(state) for state in states}
-    nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
-    nx.draw_networkx_labels(G, pos, font_weight=2)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels)
-    plt.axis('off')
-    plt.show()
-
-    write_dot(G, 'mc.dot')
+    pass
 
 def print_graph2():
     G = nx.Graph()
@@ -107,3 +38,125 @@ def print_graph2():
     ax.margins(0.20)
     plt.axis("off")
     plt.show()
+
+
+# Gif :
+import networkx as nx
+from networkx.drawing.nx_agraph import to_agraph
+import numpy as np
+import imageio
+
+# # Markov chain parameters
+# states = [(0, 0),
+#           (1, 0),
+#           (2, 0), ]
+#
+# Q = [[0.2, 0.3, 0.5],
+#      [0.1, 0.2, 0.7],
+#      [0.3, 0.7, 0]
+#      ]
+#
+# # Sampling the markov chain over 100 steps
+# N_steps = 100
+# node_ind = 0
+# node_sel = [node_ind]
+# for i in range(N_steps):
+#     temp_ni = np.random.choice(3, p=Q[node_ind])
+#     node_sel.append(temp_ni)
+#     node_ind = temp_ni
+#
+# # Setting up network
+# G = nx.MultiDiGraph()
+# [G.add_node(s, style='filled', fillcolor='white', shape='circle', fixedsize='true', width=0.5) for s in states]
+#
+# labels = {}
+# edge_labels = {}
+#
+# for i, origin_state in enumerate(states):
+#     for j, destination_state in enumerate(states):
+#         rate = Q[i][j]
+#         if rate > 0:
+#             G.add_edge(origin_state, destination_state, weight=rate, label="{:.02f}".format(rate), len=2)
+#
+# A = to_agraph(G)
+# A.layout()
+# A.draw('test.png')
+
+# # Setting up node color for each iteration
+# for k in range(N_steps):
+#     for i, n in enumerate(G.nodes(data=True)):
+#         if i == node_sel[k]:
+#             n[1]['fillcolor'] = 'blue'
+#         else:
+#             n[1]['fillcolor'] = 'white'
+#
+#     A = to_agraph(G)
+#     A.layout()
+#     A.draw('net_' + str(k) + '.png')
+#
+# # Create gif with imageio
+# images = []
+# filenames = ['net_' + str(k) + '.png' for k in range(N_steps)]
+# for filename in filenames:
+#     images.append(imageio.v2.imread(filename))
+# imageio.mimsave('markov_chain.gif', images, fps=3)
+
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Define the states
+states = ["S0", "S1", "S2"]
+
+# Define the actions
+actions = ["a", "b"]
+color_actions = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow']
+actual_state_color = '#606060'
+
+# Define the transition probabilities
+P = {
+    "S0": {
+        "S0": 0.3, "S1": 0.7
+    },
+    "S1": {
+        "a": {"S0": 0.6, "S1": 0.4},
+        "b": {"S1": 0.2, "S2": 0.8},
+    },
+    "S2": {
+        "a": {"S0": 0.5, "S1": 0.5},
+        "b": {"S1": 0.9, "S2": 0.1},
+    },
+}
+
+# Create the graph
+G = nx.DiGraph()
+
+# Add the states as nodes
+G.add_nodes_from(states)
+
+# Add the actions as edges
+for state in states:
+    if list(P[state].keys())[0] in actions:  # if there are actions
+        for action in actions:
+            for next_state in P[state][action]:
+                prob = P[state][action][next_state]
+                if prob > 0:
+                    G.add_edge(state, next_state, label=f"{action} ({prob:.2f})", len=4)
+    else:
+        for next_state in P[state]:
+            prob = P[state][next_state]
+            if prob > 0:
+                G.add_edge(state, next_state, label=f"({prob:.2f})", len=4, color='black')
+
+B = to_agraph(G)
+B.layout()
+B.draw('test2.png')
+
+# Draw the graph
+# pos = nx.spring_layout(G)
+# nx.draw_networkx(G, pos, with_labels=True)
+# edge_labels = nx.get_edge_attributes(G, "label")
+# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+# plt.show()
+
--- MDP-parser/mdp.py
+++ MDP-parser/mdp.py
@@ -2,7 +2,7 @@ from antlr4 import *
 from gramLexer import gramLexer
 from gramListener import gramListener
 from gramParser import gramParser
-from gramPrint import print_graph, print_graph2
+from gramPrint import print_graph
 import sys
 
 import numpy as np
@@ -14,25 +14,25 @@ class Etat():
     def __init__(self, nom, id, transitions) -> None:
         self.id = id
         self.nom = nom
-        self.transitions = transitions # Dictionaire:  transitions[decision] = ligne matrice
+        self.transitions = transitions  # Dictionaire:  transitions[decision] = ligne matrice
         self.have_decision = False
 
 class gramPrintListener(gramListener):
 
-    def __init__(self, dictt = {'transact_type1': [], 'transact_type2': [], 'actions' : [], 'etats': []}):
+    def __init__(self, dictt={'transact_type1': [], 'transact_type2': [], 'actions': [], 'etats': []}):
         #self.props = dictt
         pass
 
     def enterDefstates(self, ctx):
-        #print("States: %s" % str([str(x) for x in ctx.ID()]))
-        #self.props['etats'] = ([str(x) for x in ctx.ID()])
+        # print("States: %s" % str([str(x) for x in ctx.ID()]))
+        # self.props['etats'] = ([str(x) for x in ctx.ID()])
 
         for etat, id in zip([str(x) for x in ctx.ID()], range(len([str(x) for x in ctx.ID()]))):
-            etats[etat] = (Etat(nom = etat, id = id, transitions = {}))
+            etats[etat] = (Etat(nom=etat, id = id, transitions = {}))
 
     def enterDefactions(self, ctx):
-        #print("Actions: %s" % str([str(x) for x in ctx.ID()]))
-        #self.props['actions'] = [str(x) for x in ctx.ID()]
+        # print("Actions: %s" % str([str(x) for x in ctx.ID()]))
+        # self.props['actions'] = [str(x) for x in ctx.ID()]
         pass
 
     def enterTransact(self, ctx):
@@ -40,8 +40,8 @@ class gramPrintListener(gramListener):
         dep = ids.pop(0)
         act = ids.pop(0)
         weights = [int(str(x)) for x in ctx.INT()]
-        #print("Transition from " + dep + " with action "+ act + " and targets " + str(ids) + " with weights " + str(weights))
-        #self.props['transact_type1'].append([dep, act, ids, weights])
+        # print("Transition from " + dep + " with action "+ act + " and targets " + str(ids) + " with weights " + str(weights))
+        # self.props['transact_type1'].append([dep, act, ids, weights])
 
         etats[dep].transitions[act] = self.make_weights(ids, weights)
         etats[dep].have_decision = True
@@ -50,8 +50,8 @@ class gramPrintListener(gramListener):
         ids = [str(x) for x in ctx.ID()]
         dep = ids.pop(0)
         weights = [int(str(x)) for x in ctx.INT()]
-        #print("Transition from " + dep + " with no action and targets " + str(ids) + " with weights " + str(weights))
-        #self.props['transact_type2'].append([dep, ids, weights])
+        # print("Transition from " + dep + " with no action and targets " + str(ids) + " with weights " + str(weights))
+        # self.props['transact_type2'].append([dep, ids, weights])
 
         etats[dep].transitions['MC'] = self.make_weights(ids, weights)
 
@@ -66,21 +66,29 @@ class gramPrintListener(gramListener):
 
         return array/array.sum()
 
+
 def main():
 
-    global chaine # séquence d'états qui ont été faits
+    global chaine  # séquence d'états qui ont été faits
     global etats
 
     etats = {}
     chaine = []
 
-    lexer = gramLexer(FileStream("ex.mdp"))
+    lexer = gramLexer(FileStream("MDP-parser/ex.mdp"))
     stream = CommonTokenStream(lexer)
     parser = gramParser(stream)
     tree = parser.program()
     printer = gramPrintListener()
     walker = ParseTreeWalker()
     p = walker.walk(printer, tree)
+
+    print("-" * 10)
+    print(f"etats: {etats}")
+    print("-" * 10)
+    print(f"chaine: {chaine}")
+    print("-" * 10)
+
 
     print('Would you like to simulate it by choosing each action, simulating randomly or defining a positional opponent? Type 1 or 2 or 3 respectively.')
     choice = int(input('Your choice (1 or 2 or 3):'))
@@ -96,14 +104,14 @@ def main():
         adv = define_adversaire(etats)
         simulation_adv(etats, adv, n)
 
-
-    print([k.nom for k in chaine])
+    print([k.nom for k in chaine])  # print all selected states
     
     # print_graph()
-    print_graph2()
+    # print_graph2()
+    print_graph3()
     
     
-def simulation_choice(etats, n = 10):
+def simulation_choice(etats, n=10):
 
     chaine.append(min(etats.values(), key=lambda obj: obj.id))
     departure = chaine[-1]
@@ -133,7 +141,8 @@ def simulation_choice(etats, n = 10):
         departure = obj[0]
         chaine.append(departure)
 
-def simulation_rand(etats, n = 10):
+
+def simulation_rand(etats, n=10):
 
     chaine.append(min(etats.values(), key=lambda obj: obj.id))
     departure = chaine[-1]
@@ -151,7 +160,6 @@ def simulation_rand(etats, n = 10):
             arrival_id = choose_state(random_transitions)
             filtered_dict = {k: v for k, v in etats.items() if v.id == arrival_id}
 
-
         else:
 
             transitions = departure.transitions['MC']
@@ -163,7 +171,8 @@ def simulation_rand(etats, n = 10):
         departure = obj[0]
         chaine.append(departure)
 
-def simulation_adv(etats, adv, n = 10):
+
+def simulation_adv(etats, adv, n=10):
     chaine.append(min(etats.values(), key=lambda obj: obj.id))
     departure = chaine[-1]
 
@@ -172,7 +181,6 @@ def simulation_adv(etats, adv, n = 10):
         print(f'The current state is {departure.nom}')
 
         if departure.have_decision:
-
 
             key = adv[departure.nom]
             transitions = departure.transitions[key]
@@ -192,10 +200,12 @@ def simulation_adv(etats, adv, n = 10):
         departure = obj[0]
         chaine.append(departure)
 
+
 def choose_state(weights):
     actions = list(range(len(weights)))
     choice = random.choices(actions, weights=weights, k=1)
     return choice[0]
+
 
 def define_adversaire(etats):
 
@@ -207,6 +217,7 @@ def define_adversaire(etats):
         adv[etat.nom] = str(input('Your choice for your adversiare is: '))
     
     return adv
+
 
 if __name__ == '__main__':
     main()
